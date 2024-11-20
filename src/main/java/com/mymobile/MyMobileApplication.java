a4471ec4ccd9966ef9211c70de5cb2df95bf6117
User <User@ADITLTP026>
2024-11-20 11:39:01 +0530

    Adding project Structure

---
+++ src/main/java/com/mymobile/MyMobileApplication.java
@@ -0,0 +1,13 @@
+package com.mymobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymobile/MyMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
